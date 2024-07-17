36f307e165e2e1ec9a2c99da0274a36bdde49b2a
ARYANPATILSOLOCODER11 <aryanpformal@gmail.com>
2024-07-17 17:17:10 +0530

    cleared

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,7 @@
 export default function Home() {
   return (
    <main>
-    <h1>AI powered Meet</h1>
-    <h1>This is Aryan </h1>
-    <h1>This is Aryan from test branch </h1>
-    <h1>Teams Created Succesfully</h1>
-    <h1>pushed to main Succesfully</h1>
-    <h1>Aryan from dev branch</h1>
+   
    </main>
   );
 }
