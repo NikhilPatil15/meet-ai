00054000da037ac632fff86b82f1b396f7b20620
NikhilPatil15 <137986217+NikhilPatil15@users.noreply.github.com>
2024-07-17 16:27:20 +0530

    Teams created succesfully and succesfully pushed to test branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <h1>AI powered Meet</h1>
     <h1>This is Aryan </h1>
     <h1>This is Aryan from test branch </h1>
-
+    <h1>Teams Created Succesfully</h1>
    </main>
   );
 }
