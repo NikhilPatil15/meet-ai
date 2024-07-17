ab62c3cbf0da0c5ce5076476469e63eb3035a7c1
ARYANPATILSOLOCODER11 <aryanpformal@gmail.com>
2024-07-17 16:31:13 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <h1>This is Aryan </h1>
     <h1>This is Aryan from test branch </h1>
     <h1>Teams Created Succesfully</h1>
+    <h1>pushed to main Succesfully</h1>
    </main>
   );
 }
