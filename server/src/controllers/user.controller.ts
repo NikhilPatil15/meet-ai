b7ecf58e2a359856db53fb3a088bf704f003c90a
NikhilPatil15 <137986217+NikhilPatil15@users.noreply.github.com>
2024-07-26 16:52:19 +0530

    user controller added and database connected successfully with server creation

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,136 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/apiError";
+import { ApiResponse } from "../utils/apiResponse";
+import { asyncHandler } from "../utils/asyncHandler";
+import { Request, Response } from "express";
+
+const options = {
+  httpOnly: true,
+  secure: true,
+};
+
+const generateAccessandRefreshToken = async (userId: any) => {
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new ApiError(
+        401,
+        "user does not exist so tokens can not be created!"
+      );
+    }
+    const accessToken = user?.generateAccessToken();
+    const refreshToken = user?.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(401, "Something went wrong while creating the tokens!");
+  }
+};
+
+const registerUser = asyncHandler(async (req: Request, res: Response) => {
+  const { userName, fullName, email, password } = req.body;
+
+  if (
+    [userName, fullName, email, password].some((value) => value?.trim() === "")
+  ) {
+    throw new ApiError(401, "All fields are necessary!");
+  }
+
+  const userExists = await User.findOne({
+    $or: [{ userName }, { email }],
+  });
+
+  if (userExists) {
+    throw new ApiError(401, "Username or email already exists!");
+  }
+
+  const user = await User.create({
+    userName,
+    fullName,
+    email,
+    password,
+  });
+
+  const displayUser = await User.findById(user._id)?.select(
+    "-password -refreshToken"
+  );
+
+  if (!displayUser) {
+    throw new ApiError(401, "Something went wrong while creating user!");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, displayUser, "user created successfully!"));
+});
+
+const loginUser = asyncHandler(async (req: Request, res: Response) => {
+  const { userName, password } = req.body;
+
+  if ([userName, password].some((value) => value.trim() === "")) {
+    throw new ApiError(401, "All fields are necessary!");
+  }
+
+  const user = await User.findOne({ userName: userName });
+
+  if (!user) {
+    throw new ApiError(401, "user does not exist!");
+  }
+
+  const checkPassword = await user.isPasswordCorrect(password);
+
+  if (!checkPassword) {
+    throw new ApiError(400, "Incorrect Password!");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessandRefreshToken(
+    user?._id
+  );
+
+  const loggedUser = await User.findById(user?._id).select(
+    "-password -refreshToken"
+  );
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refrehToken", refreshToken, options)
+    .json(
+      new ApiResponse(200, { user: loggedUser }, "user logged in successfully!")
+    );
+});
+
+const logoutUser = asyncHandler(async (req: any, res: Response) => {
+  const user = req.user;
+
+  if (!user) {
+    throw new ApiError(400, "user should be logged in first!");
+  }
+  await User.findByIdAndUpdate(
+    user?._id,
+    { $unset: { refreshToken: 1 } },
+    { new: true }
+  );
+
+  res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "user loggedout successfully"));
+});
+
+const getUser = asyncHandler(async (req: any, res: Response) => {
+  const user = req.user;
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, { FetchedUser: user }, "user fetched successfully!")
+    );
+});
+
+export { registerUser, loginUser, logoutUser,getUser };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDatabase } from "./Database/db";
 
-console.log("Hello")
+/* Backend server initialised */
+const app = express();
+
+/* To use .env variables throughout our code */
+dotenv.config({ path: "./.env" });
+
+/* Allow cross-origin requests from specified origin and include credentials (cookies, authorization headers, etc.) */
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+
+/* Parse incoming requests with JSON payloads*/
+app.use(express.json());
+
+/* Connected the database */
+connectDatabase().then(() => {
+  console.log("Database connected successfully!");
+});
+
+/* Parse Cookie header and populate req.cookies with an object keyed by the cookie names*/
+app.use(cookieParser());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running at: http://localhost:${port}`);
+});
+
+/* HealthCheck route */
+app.get("/", (req, res) => {
+  res.send("Meet AI backend!");
+});
