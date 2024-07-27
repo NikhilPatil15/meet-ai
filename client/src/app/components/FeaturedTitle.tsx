9c36b1ce7e6972237453b9b98497831016657de6
Avantika Patil <avantikapatil202@gmail.com>
2024-07-28 01:43:58 +0530

    second commit

--- client/src/app/components/FeaturedTitle.tsx
+++ client/src/app/components/FeaturedTitle.tsx
@@ -10,7 +10,9 @@ function FeaturedTitle() {
         fill="white"
       /> */}
       <div className='p-2 md:p- relative z-10 w-full text-center'>
-         <h1>Be more productive in work , life and productivity for meeting summary. </h1>
+         <h1 className={cn(
+        "text-base sm:text-3xl md:text-3xl lg:text-4xl font-bold text-center dark:text-white max-w-lg mx-auto leading-relaxed"
+      )}>Be more productive in work , life and productivity for meeting summary. </h1>
         <p className='mt-5 text-base md:text-lg text-neutral-300 max-w-lg mx-auto leading-relaxed '>
           Our meeting Summary tool is your key to being more productive,  Whether at work or in your personal lifr. it helps you stay organized and efficient . ensuring you never forget importatnt details , tasks or deadlines.
         </p>
