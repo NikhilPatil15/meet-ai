19d90e54a51b73f211abbe6981c19fea4252e13e
NikhilPatil15 <137986217+NikhilPatil15@users.noreply.github.com>
2024-07-29 01:13:53 +0530

    handleSubmit function created!

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -1,6 +1,27 @@
+'use client'
 import Image from "next/image";
-
+import { FormEvent, useState } from "react";
+import axios from 'axios'
+import { BASE_URL } from "@/constants/BackendURL";
 
 export default function Home() {
+
+  const [userName,setUserName] = useState('')
+  const [password,setPassword] = useState('')
+
+  const handleSubmit = async (e:FormEvent) => {
+    e.preventDefault()
+
+    const data = {
+      userName:userName,
+      password:password
+    }
+
+    await axios.post(`${BASE_URL}/user/login`,data)
+    .then(response=>console.log("Response: ",response))
+    .catch(error=>console.log("Error while requesting the login url: ",error)
+    )
+
+  }
   return <div className='bg-blue-900'>Login page</div>;
 }
--- client/src/app/auth/register/page.tsx
+++ client/src/app/auth/register/page.tsx
@@ -1,5 +1,28 @@
-import Image from "next/image";
+'use client'
+import { FormEvent, useState } from "react";
+import axios from 'axios'
+import { BASE_URL } from "@/constants/BackendURL";
 
 export default function Home() {
+  const [fullName,setFullName] = useState('')
+  const [userName,setUserName] = useState('')
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  
+  const handleSubmit =async (e:FormEvent) => {
+    e.preventDefault()
+
+    const data = {
+      userName:userName,
+      fullName:fullName,
+      email:email,
+      password:password
+    }
+
+   await axios.post(`${BASE_URL}/user/register`,data)
+    .then(response => console.log("Response: ", response))
+    .catch(error => console.log("Error while requesting to register route: ",error))
+    
+  }
   return <div>Register Page</div>;
 }
