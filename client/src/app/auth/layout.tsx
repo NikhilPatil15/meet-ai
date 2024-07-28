11a61055ca6b7137e2eeb3a10f76b81bfb9e2151
ARYANPATILSOLOCODER11 <aryanpformal@gmail.com>
2024-07-29 00:22:34 +0530

    done

--- client/src/app/auth/layout.tsx
+++ client/src/app/auth/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import '@/'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
             <body className={inter.className}>
+              <h1>layout for auth</h1>
             {children}
             </body>
     </html>
