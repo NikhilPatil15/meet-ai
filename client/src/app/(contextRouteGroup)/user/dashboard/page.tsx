253324cf552429a6bb2297c3cc7c7c6f12321198
Avantika Patil <avantikapatil202@gmail.com>
2024-10-06 21:04:46 +0530

    fix:design issue

--- client/src/app/(contextRouteGroup)/user/dashboard/page.tsx
+++ client/src/app/(contextRouteGroup)/user/dashboard/page.tsx
@@ -13,7 +13,7 @@ function Home() {
     <section className="flex size-full flex-col gap-5 text-white h-full">
        <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
      <HomePage/>
-     <EditProfile></EditProfile>
+     {/* <EditProfile></EditProfile> */}
       </div>
     </section>
     
