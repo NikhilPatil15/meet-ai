f1f1ba8dd9620c6a5c7364c057fdb909ca179d4a
Avantika Patil <avantikapatil202@gmail.com>
2024-10-06 14:37:37 +0530

    feat: add EditProfile component

--- client/src/app/(contextRouteGroup)/user/dashboard/page.tsx
+++ client/src/app/(contextRouteGroup)/user/dashboard/page.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
  // Ensure correct path
 import TopBar from '@/components/DashBoard/Slidebar/TopBar'; // Ensure correct path
 import HomePage from "@/components/DashBoard/Homepage/HomePage"; // Ensure correct path
+import EditProfile from '@/components/DashBoard/EditProfile/EditProfile';
 
 function Home() {
   
@@ -12,6 +13,7 @@ function Home() {
     <section className="flex size-full flex-col gap-5 text-white h-full">
        <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
      <HomePage/>
+     <EditProfile></EditProfile>
       </div>
     </section>
     
