16808753cfb43790c4cb2898591546e8edfc9f10
CodeShotsAryan <aryancse1@gmail.com>
2024-10-06 15:53:54 +0530

    MH

--- client/src/app/(contextRouteGroup)/user/dashboard/history/page.tsx
+++ client/src/app/(contextRouteGroup)/user/dashboard/history/page.tsx
@@ -2,21 +2,35 @@
 import React,{useEffect,useState}from "react"
 import axios from 'axios'
 import Box from '@mui/material/Box'
+import MeetingCard from "@/components/DashBoard/meetingCard"
+import { Card } from "@mui/material"
 
 export default function MeetingHistory(){
     const [meetings , setMeetings] = useState([])
 
     const fetchMeetingsHistory = async()=>{
         try {
-            
+                const response = await axios.get('http://localhost:5000/api/v1/user/get-meeting-history')
+                setMeetings(response.data)
+
         } catch (error) {
             console.error("Error Fetching meeting History")
             
         }
     }
+
+    useEffect(()=>{
+        fetchMeetingsHistory();
+    },[])
     return(
         <div>
-            This is meeting history page
+            <Box className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
+      {meetings.map((meeting) => (
+        <>
+
+        </>
+      ))}
+    </Box>
         </div>
     )
 }
