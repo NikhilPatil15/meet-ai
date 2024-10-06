5c8dd9a400ecc9f6c4151ae7d6668c1c3e5fff20
CodeShotsAryan <aryancse1@gmail.com>
2024-10-06 14:33:52 +0530

    Private Meeting

--- client/src/constants/StateCardData.ts
+++ client/src/constants/StateCardData.ts
@@ -1,15 +1,15 @@
+import add from '@/assets/icons/add-meeting.svg';
+import join from '@/assets/icons/join-meeting.svg';
+import schedule from '@/assets/icons/schedule.svg';
+import recording from '@/assets/icons/share.svg';
 
-import add from '@/assets/icons/add-meeting.svg'
-import join from '@/assets/icons/join-meeting.svg'
-import schedule from '@/assets/icons/schedule.svg'
-import recording from '@/assets/icons/share.svg'
 export interface StateCardItem {
   title: string;
   description: string;
   iconColor: string;
   iconbg: string;
   img: any;
-  isState: 'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting';
+  isState: 'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | 'isPrivateMeeting';
 }
 
 export const StateCardData: StateCardItem[] = [
@@ -27,7 +27,7 @@ export const StateCardData: StateCardItem[] = [
     iconColor:"#4cceac",
     iconbg: "#0F67B1",
     img: join,
-    isState: 'isInstantMeeting',
+    isState: 'isJoiningMeeting',
   },
   {
     title: "Schedule Meeting",
@@ -35,14 +35,14 @@ export const StateCardData: StateCardItem[] = [
     iconColor: "#ffffff",
     iconbg: "#921A40",
     img: schedule,
-    isState: 'isInstantMeeting',
+    isState: 'isScheduleMeeting',
   },
-  // {
-  //   title: "Share screen",
-  //   description: "Share your screen in a meeting",
-  //   iconColor: "#ffffff",
-  //   iconbg: "#337357",
-  //   img: recording,
-  //   isState: 'isInstantMeeting',
-  // },
+  {
+    title: "Private Meeting",
+    description: "Start a private meeting",
+    iconColor: "#ffffff",
+    iconbg: "#ff5722", // Custom color for the private meeting
+    img: schedule, // You can change this to another icon
+    isState: 'isPrivateMeeting',
+  },
 ];
