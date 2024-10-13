2ee307181d302c5256fb6672e9e06be5b756757b
NikhilPatil15 <137986217+NikhilPatil15@users.noreply.github.com>
2024-10-13 11:37:03 +0530

    minor changes

--- server/src/controllers/summary.controller.ts
+++ server/src/controllers/summary.controller.ts
@@ -1,27 +1,35 @@
-import { Response } from "express"
-import Meeting, { IMeeting } from "../models/meeting.model"
-import { asyncHandler } from "../utils/asyncHandler"
-import { ApiResponse } from "../utils/apiResponse"
+import { Response } from "express";
+import Meeting, { IMeeting } from "../models/meeting.model";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiResponse } from "../utils/apiResponse";
+import { isValidObjectId } from "mongoose";
+import { ApiError } from "../utils/apiError";
 
 const addDialogue = asyncHandler(async (req: any, res: Response) => {
-    const { dialogue, meetingId } = req.body;
-    if (!dialogue || !meetingId) {
-      return res.status(400).json(new ApiResponse(400, null, "Dialogue or Meeting ID missing"));
-    }
-    const meeting: any = await Meeting.findById(meetingId);
-    if (!meeting) {
-      return res.status(404).json(new ApiResponse(404, null, "Meeting not found"));
-    }
-    if (!meeting.dialogues || meeting.dialogues === "") {
-      meeting.dialogues = dialogue;
-    } else {
-      meeting.dialogues += `\n${dialogue}`;
-    }
-    await meeting.save();
-  
-    return res.status(200).json(new ApiResponse(200, meeting, "Dialogue appended successfully"));
-  });
+  const { dialogue, meetingId } = req.body;
+  if (!dialogue || !meetingId) {
+    throw new ApiError(400, "Dialogue or meeting id is missing!")
+  }
+  if (!isValidObjectId(meetingId)) {
+    throw new ApiError(400, "Invalid meeting id!");
+  }
 
-export {
-    addDialogue
-}
+  const meeting: any = await Meeting.findById(meetingId);
+  if (!meeting) {
+    return res
+      .status(404)
+      .json(new ApiResponse(404, null, "Meeting not found"));
+  }
+  if (!meeting.dialogues || meeting.dialogues === "") {
+    meeting.dialogues = dialogue;
+  } else {
+    meeting.dialogues += `\n${dialogue}`;
+  }
+  await meeting.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, meeting, "Dialogue appended successfully"));
+});
+
+export { addDialogue };
