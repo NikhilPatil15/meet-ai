54b1813a8dc4501d95ad2e006c49b036b8784fdc
Shree2124 <shreealasande@gmail.com>
2024-11-03 00:06:49 +0530

    feat: integrate today's schedule api

--- client/src/components/DashBoard/Upcoming/Upcoming.tsx
+++ client/src/components/DashBoard/Upcoming/Upcoming.tsx
@@ -9,6 +9,7 @@ import { BarChart } from "@/components/specifics/Chart";
 const Upcoming = () => {
   const [loading, setLoading] = useState(true);
   const [upcoming, setUpcoming] = useState<any>(null);
+  const [todays, setTodays] = useState<any>(null);
   const [next7DaysDetails, setNext7DaysDetails] = useState<any>([]);
 
   const fetchUpcomingMeetings = async () => {
@@ -42,6 +43,19 @@ const Upcoming = () => {
     }
   };
 
+  const fetchTodaysSchedule = async () => {
+    try {
+      setLoading(true);
+      const res: any = await axiosInstance.get("/user/get-todays-schedule");
+      setTodays(res.data.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     const loadContent = async () => {
       await new Promise((resolve) => setTimeout(resolve, 1000)); // Delay of 1 second
@@ -51,6 +65,7 @@ const Upcoming = () => {
     loadContent();
     fetchUpcomingMeetings();
     fetchNext7DaysMeetingDetails();
+    fetchTodaysSchedule();
   }, []);
   return (
     <div>
@@ -98,18 +113,32 @@ const Upcoming = () => {
         <h1 className="mb-4 text-3xl font-bold">Today</h1>
       </Box>
       <Box
-        mb="20px"
-        p="10px"
-        className="h-full bg-gray-800 rounded-xl shadow-lg flex flex-col justify-between"
+        mt="25px"
+        className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4"
       >
-        <MeetingCard
-          title={"testing today"}
-          buttonText="Join Meet"
-          handleClick={() => {}}
-          type={"scheduled"}
-          roomId={"todayID"}
-          description={"Just do your part"}
-        />
+        {loading
+          ? Array.from({ length: 4 }).map((_, index) => (
+              <CardSkeleton key={index} />
+            ))
+          : todays?.map((meeting: any, index: any) => (
+              <Box
+                key={index}
+                mb="20px"
+                p="10px"
+                className="h-full bg-gray-800 rounded-xl shadow-lg flex flex-col justify-between"
+              >
+                <MeetingCard
+                  title={meeting.title}
+                  buttonText="Join Meet"
+                  handleClick={() => {}}
+                  type={meeting.type}
+                  roomId={meeting.roomId}
+                  description={meeting.description}
+                  participants={meeting.participants}
+                  scheduledTime={meeting.scheduledTime}
+                />
+              </Box>
+            ))}
       </Box>
 
       {/* Upcoming Meetings */}
@@ -131,7 +160,7 @@ const Upcoming = () => {
           ? Array.from({ length: 4 }).map((_, index) => (
               <CardSkeleton key={index} />
             ))
-          : upcoming?.map((meeting:any, index:any) => (
+          : upcoming?.map((meeting: any, index: any) => (
               <Box
                 key={index}
                 mb="20px"
